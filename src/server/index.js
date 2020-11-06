2e67bc0f893507c1cf1227a9ff74b424b4591fe6
sumukshashidhar <sumukuuu@gmail.com>
2020-11-07 00:04:59 +0530

    initial commit

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require('./config/logger')
+const morgan = require('morgan')
+const rfs = require('rotating-file-stream') // version 2.x
+const path = require('path')
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+var accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'log')
+})
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+require("./routes")(app);
+
+
+var PORT_CHECK = 8000
+
+app.listen(3000, "0.0.0.0", function (req,res) {
+    logger.info("Server Started");
+});
---
+++ src/server/routes.js
@@ -0,0 +1,3 @@
+module.exports  = function(app) {
+    require('./routes/main.js')(app);
+};
