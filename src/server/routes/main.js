685c0e692ecc6b3432085d724bc2cb4c7ef95eb3
sumukshashidhar <sumukuuu@gmail.com>
2020-11-07 23:20:39 +0530

    added some more code

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -1,7 +1,30 @@
+const keysaver = require("./../controllers/key_func");
 module.exports = function(app) {
     app.get('/', function(req, res) {
         res.status(200).json({
             "message":"Server is Online"
         })
+    });
+
+    app.post('/savekey', function(req, res) {
+        if(req.body.key !== undefined) {
+            const resp = keysaver.saveKey(req.body.key)
+            if(resp) {
+                // means that it successfully saved
+                res.status(200).json({
+                    "message": "Saved the key"
+                })
+            }
+            else {
+                res.status(500).json({
+                    "message":"Abort! Could not find key"
+                })
+            }
+        }
+        else {
+            res.status(403).json({
+                "message":"Did not send the key"
+            })
+        }
     });
 }
